4e6ac7b61694b51d6de0b392c87ba93f225fe287
WenDev <wendev1024@icloud.com>
2019-12-31 15:18:22 +0800

    完成了部分前端页面的编写，但因为Spring Security未设置所以暂时不能访问

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WenDev-Web
 
-使用`Spring Boot`和`MongoDB`构建的我的个人网站。
+使用`Spring Boot`和`MongoDB`构建的个人网站框架。
+
+# 技术栈
+
++ Spring Boot
++ Spring Data MongoDB
++ Spring Security
++ MongoDB
+
+# 版本更新日志
+
+## 当前版本（0.0.1）
+
++ 完成了用户相关的服务：登录、注册、删除、修改信息、查找全部用户等
---
+++ src/main/java/site/wendev/web/controller/UserController.java
@@ -0,0 +1,49 @@
+package site.wendev.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import site.wendev.web.domain.User;
+import site.wendev.web.service.UserService;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HttpSession session;
+
+    @GetMapping(value = "/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping(value = "/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(String username, String password, RedirectAttributes attributes) {
+        if (username == null || password == null) {
+            attributes.addFlashAttribute("errInfo", "用户名或密码不能为空！");
+            return "redirect:/login";
+        }
+
+        User user = userService.login(username, password);
+
+        if (user != null) {
+            session.setAttribute("userId", user.getId());
+            System.out.printf("%s : %s 已经成功登录", user.getUsername(), user.getPassword());
+            return "redirect:/index";
+        } else {
+            attributes.addFlashAttribute("errInfo", "登录失败：用户名或密码错误");
+            return "redirect:/login";
+        }
+    }
+}
